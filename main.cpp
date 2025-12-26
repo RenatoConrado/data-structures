a53da86f4399c66eed589b9d52983b34eefacd68
Renato <renatoconrado38@gmail.com>
2025-12-26 17:16:49 -0300

    linked list

--- main.cpp
+++ main.cpp
@@ -1,3 +1,44 @@
+#define LINKED_LIST
+#ifdef LINKED_LIST
+#include <iostream>
+using namespace std;
+
+/**
+  {} = Node;
+  [] = referência de memoria;
+  head = primeiro endereço, ele nos dá acesso a lista toda;
+  [204, 217, 232, 252];
+  { 6, 217 } -> { 5, 232 } -> { 2, 252 } -> { 3, nullptr };
+*/
+struct Node {
+  int data;
+  Node *next;
+};
+
+int main(int argc, const char **argv) {
+  Node primeiro{6};
+  Node segundo{5};
+  Node terceiro{2};
+  Node quarto{3, nullptr};
+
+  primeiro.next = &segundo;
+  segundo.next = &terceiro;
+  terceiro.next = &quarto;
+
+  const int LENGTH = 4;
+  Node linkedList[LENGTH]{primeiro, segundo, terceiro, quarto};
+  for (short i = 0; i < LENGTH; i++) {
+    cout << "Node[" << i << "] = { data: " << linkedList[i].data << ", "
+         << linkedList[i].next << " }" << endl;
+  }
+
+  return 0;
+}
+
+#endif // LINKED_LIST
+
+// #define TEMPLATE
+#ifdef TEMPLATE
 #include <iostream>
 #include <string>
 
@@ -26,9 +67,12 @@ int main(int argc, const char **argv) {
   cout << "array[0] = " << stringArray.at(0) << endl;
   return 0;
 }
+#endif // TEMPLATE
 
+// #define COUT_VS_PRINTF
 #ifdef COUT_VS_PRINTF
 #include <cstdio>
+#include <iostream>
 int main() {
   short i;
   const char *arrAlgo[4]{"algo1", "algo2"};
