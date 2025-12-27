36e9fc62aeb1b4ce61427107d85622060883d26b
Renato <renatoconrado38@gmail.com>
2025-12-27 16:11:21 -0300

    added exclusion to linked list
    Signed-off-by: Renato <renatoconrado38@gmail.com>

--- main.cpp
+++ main.cpp
@@ -26,12 +26,31 @@ void iterate(Node *head) {
 
 /**
  * Insertion: time complexity: O(1) por que temos a referencia do elemento;
- * [1: 204, 2: 217, 3: 232, 4: 252, 5: **264**];
+ * [1: 204, 2: 217, 3: **264**, 4: 232, 5: 252];
  * {6, 217} -> {5, 264} -> **{8, 232}** -> {2, 252} -> {3, nullptr};
  */
 void insertAfter(Node *afterNode, Node *newNode) {
   newNode->next = afterNode->next;
   afterNode->next = newNode;
+}
+
+/**
+ * exclusion: time complexity: entre O(1) primeiro node, até O(n) ultimo node;
+ * por que temos que atravessar a estrutura até achar o elemento;
+ * [1: 204, 2: 217, 3: **264**, 4: 232, 5: 252];
+ * {6, 217} -> {5, 264} -> **{8, 232}** -> {2, 252} -> {3, nullptr};
+ */
+void removeAt(Node *&head, Node *at) {
+  if (head == at) {
+    head = head->next;
+    return;
+  }
+
+  for (Node *node = head; node != nullptr; node = node->next)
+    if (node->next == at) {
+      node->next = node->next->next;
+      return;
+    }
 }
 
 int main() {
@@ -52,6 +71,14 @@ int main() {
   Node newThird{8};
   insertAfter(&second, &newThird);
   iterate(head);
+
+  cout << endl << "Removing a node" << endl;
+  removeAt(head, &first);
+  iterate(head);
+
+  cout << "Head mudou, mas &first ainda existe" << endl
+       << "Head{" << head->data << ", " << head->next << "}" << endl
+       << "First{" << first.data << ", " << first.next << "}" << endl;
 
   return 0;
 }
