055139acf664963691c3082b4e4ef6655fa03de1
Renato <renatoconrado38@gmail.com>
2025-12-27 17:48:16 -0300

    added dynamic memory management to linked list
    Signed-off-by: Renato <renatoconrado38@gmail.com>

--- main.cpp
+++ main.cpp
@@ -3,137 +3,81 @@
 #include <iostream>
 using namespace std;
 
-/**
- * {} = Node;
- * [] = referência de memória;
- * [204, 217, 232, 252];
- * {6, 217} -> {5, 232} -> {2, 252} -> {3, nullptr};
- */
+/*
+{} = Node;
+[] = referência de memória;
+[204, 217, 232, 252];
+{6, 217} -> {5, 232} -> {2, 252} -> {3, nullptr}; */
 struct Node {
   int data;
-  Node *next;
+  Node *next = nullptr;
 };
 
 /**
- * Iteration: time complexity: O(n), space: O(1);
- * @param head Primeiro elemento, ele nos dá acesso à lista toda
- */
+Iteration: time complexity: O(n), space: O(1);
+@param head Primeiro elemento, ele nos dá acesso à lista toda */
 void iterate(Node *head) {
   for (Node *node = head; node != nullptr; node = node->next)
     cout << "Node[" << node << "] = { data: " << node->data
          << ", next: " << node->next << " }" << endl;
 }
 
-/**
- * Insertion: time complexity: O(1) por que temos a referencia do elemento;
- * [1: 204, 2: 217, 3: **264**, 4: 232, 5: 252];
- * {6, 217} -> {5, 264} -> **{8, 232}** -> {2, 252} -> {3, nullptr};
- */
+/*
+Insertion: time complexity: O(1) por que temos a referencia do elemento;
+[1: 204, 2: 217, 3: **264**, 4: 232, 5: 252];
+{6, 217} -> {5, 264} -> **{8, 232}** -> {2, 252} -> {3, nullptr}; */
 void insertAfter(Node *afterNode, Node *newNode) {
   newNode->next = afterNode->next;
   afterNode->next = newNode;
 }
 
-/**
- * exclusion: time complexity: entre O(1) primeiro node, até O(n) ultimo node;
- * por que temos que atravessar a estrutura até achar o elemento;
- * [1: 204, 2: 217, 3: **264**, 4: 232, 5: 252];
- * {6, 217} -> {5, 264} -> **{8, 232}** -> {2, 252} -> {3, nullptr};
- */
+/*
+exclusion: time complexity: entre O(1) primeiro node, até O(n) ultimo node;
+por que temos que atravessar a estrutura até achar o elemento;
+[1: 204, 2: 217, 3: 264, --2-3-2--, 4: 252];
+{6, 217} -> {5, 264} -> {8, 232} --{-2-,-2-5-2-}--> {3, nullptr}; */
 void removeAt(Node *&head, Node *at) {
   if (head == at) {
     head = head->next;
-    return;
+    return delete at;
   }
 
   for (Node *node = head; node != nullptr; node = node->next)
     if (node->next == at) {
       node->next = node->next->next;
-      return;
+      return delete at;
     }
 }
 
 int main() {
-  Node first{6};
-  Node second{5};
-  Node third{2};
-  Node forth{3, nullptr};
+  Node *first = new Node{6};
+  Node *second = new Node{5};
+  Node *third = new Node{2};
+  Node *forth = new Node{3, nullptr};
 
-  first.next = &second;
-  second.next = &third;
-  third.next = &forth;
+  first->next = second; // first->next é um atalho para (*first).next
+  second->next = third;
+  third->next = forth;
 
-  Node *head = &first;
+  Node *head = first;
+
   cout << "Iteration over Linked List" << endl;
   iterate(head);
 
   cout << endl << "Inserting a new node" << endl;
-  Node newThird{8};
-  insertAfter(&second, &newThird);
+  Node *newThird = new Node{8};
+  insertAfter(second, newThird);
   iterate(head);
 
   cout << endl << "Removing a node" << endl;
-  removeAt(head, &first);
+  removeAt(head, first);
   iterate(head);
 
-  cout << "Head mudou, mas &first ainda existe" << endl
+  cout << endl
+       << "Head mudou e first foi deletado" << endl
        << "Head{" << head->data << ", " << head->next << "}" << endl
-       << "First{" << first.data << ", " << first.next << "}" << endl;
+       << "First{" << first->data << ", " << first->next << "}" << endl;
 
   return 0;
 }
 #endif // LINKED_LIST
-
-// #define TEMPLATE
-#ifdef TEMPLATE
-#include <iostream>
-#include <string>
-
-using namespace std;
-
-template <typename T> class Array {
-private:
-  short length;
-  T *array;
-
-public:
-  Array(short length) : length(length), array(new T[length]) {}
-  ~Array() { delete[] array; }
-
-  void fill(T value) {
-    for (short i = 0; i < length; i++)
-      array[i] = value;
-  }
-
-  T &at(int index) { return array[index]; }
-};
-
-int main(int argc, const char **argv) {
-  Array<string> stringArray(10);
-  stringArray.fill("algo");
-  cout << "array[0] = " << stringArray.at(0) << endl;
-  return 0;
-}
-#endif // TEMPLATE
-
-// #define COUT_VS_PRINTF
-#ifdef COUT_VS_PRINTF
-#include <cstdio>
-#include <iostream>
-int main() {
-  short i;
-  const char *arrAlgo[4]{"algo1", "algo2"};
-
-  const short length = sizeof(arrAlgo) / sizeof(arrAlgo[0]);
-  arrAlgo[3] = "outra coisa";
-
-  for (i = 0; i < length; i++)
-    if (arrAlgo[i])
-      cout << "cout  Array: [" << i << "] = " << arrAlgo[i] << endl;
-
-  for (i = 0; i < length; i++)
-    if (arrAlgo[i])
-      printf("prinf Array: [%d] = %s\n", i, arrAlgo[i]);
-  return 0;
-}
-#endif // COUT_VS_PRINTF
